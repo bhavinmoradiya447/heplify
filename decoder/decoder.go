879fef57b4970f6275229aa0c1c9ae4571fcdfb6
bmoradiy <bmoradiy@cisco.com>
2025-01-20 13:51:51 +0530

    with Final

--- decoder/decoder.go
+++ decoder/decoder.go
@@ -186,7 +186,6 @@ func (d *Decoder) defragIP6(i6 layers.IPv6, i6frag layers.IPv6Fragment, t time.T
 
 func (d *Decoder) Process(data []byte, ci *gopacket.CaptureInfo) {
 
-	logp.Info("Packet received: %v", string(data))
 	if config.Cfg.Dedup {
 		if len(data) > 34 {
 			_, err := d.dedupCache.Get(data[34:])
